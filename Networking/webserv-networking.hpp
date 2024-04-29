fb7fe099499a30ebe85e180a3b097b595dbda8bb
Nick van den Dungen <nicktvdd@hotmail.com>
2024-04-29 12:50:42 +0300

    mixed up cpp and hpp

---
+++ Networking/webserv-networking.hpp
@@ -0,0 +1,8 @@
+#ifndef webserver_networking_hpp
+#define webserver_networking_hpp
+
+#include <stdio.h>
+
+#include "Networking/Sockets/webserv-sockets.hpp"
+
+#endif /* webserver_networking_hpp */
--- Networking/werbserv-networking.cpp
+++
@@ -1,8 +0,0 @@
-#ifndef webserver_networking_hpp
-#define webserver_networking_hpp
-
-#include <stdio.h>
-
-#include "Networking/Sockets/webserv-sockets.hpp"
-
-#endif /* webserver_networking_hpp */
