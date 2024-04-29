95a62374be38eca414a4f6fe9a01363f8e8fdba9
Nick van den Dungen <nicktvdd@hotmail.com>
2024-04-29 10:15:12 +0300

    Header structure

---
+++ Networking/Sockets/webserv-sockets.hpp
@@ -0,0 +1,12 @@
+#ifndef webserv_sockets_hpp
+#define webserv_sockets_hpp
+
+#include <stdio.h>
+
+#include "SimpleSocket.hpp"
+
+#include "BindingSocket.hpp"
+#include "ConnectingSocket.hpp"
+#include "ListeningSocket.hpp"
+
+#endif /* webserv_sockets_hpp */
---
+++ webserv.hpp
@@ -0,0 +1,8 @@
+#ifndef webserv_hpp
+#define webserv_hpp
+
+#include <stdio.h>
+
+#include "Networking/webserv-networking.hpp"
+
+#endif /* webserv_hpp */
